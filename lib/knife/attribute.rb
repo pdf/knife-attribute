7bff62cede339bcd41012e5796a029d78f9647b6
Peter Fern <github@obfusc8.org>
2013-11-01 15:23:24 +1100

    Initial creation via bundler

---
+++ lib/knife/attribute.rb
@@ -0,0 +1,7 @@
+require "knife/attribute/version"
+
+module Knife
+  module Attribute
+    # Your code goes here...
+  end
+end
---
+++ lib/knife/attribute/version.rb
@@ -0,0 +1,5 @@
+module Knife
+  module Attribute
+    VERSION = "0.0.1"
+  end
+end
