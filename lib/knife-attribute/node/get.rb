ca263bdfff24c745ba331b1d0a75f09e771f0b86
Peter Fern <github@obfusc8.org>
2017-08-01 13:35:14 +1000

    Allow retrieving `automatic` type values from nodes
    Fixes #3

--- lib/knife-attribute/node/get.rb
+++ lib/knife-attribute/node/get.rb
@@ -4,6 +4,15 @@ module KnifeAttribute
       include KnifeAttribute::Node::Helpers
       include KnifeAttribute::Get
 
+      def self.attribute_type_map
+        {
+          default: :default_attrs,
+          normal: :normal_attrs,
+          override: :override_attrs,
+          automatic: :automatic_attrs,
+        }
+      end
+
       banner 'knife node attribute get NODE PERIOD.SEPARATED.ATTRIBUTE (options)'
 
     end
